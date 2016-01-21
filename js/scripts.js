f4fa2e84167d89ae0cc9d971e27e0a0ae56a6e9b
Erik Tolentino and Sean Peerenboom <tolentino3575@gmail.com>
2016-01-21 10:47:37 -0800

    Create initial files

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+$("form#question").submit(function(event) {
+
+var gabe = parseInt($("input#ct").val());
+var andy = parseInt($("input#cb").val());
+// var apple = []
+// for (var druid = 0; druid < gabe; druid+=andy) {
+//   apple.push("this is a test" + "<br>");
+//   // console.log("STEPPING");
+// }
+// apple = apple.join(" ");
+// $("#message").append(apple);
+// if (gabe === andy) {
+//   $("#message").show();
+// } else {
+//   $("#panic").show();
+// }
+// var kate = []
+// kate.prototype.accumulate =function(fn){
+//     var r = [this[0]];
+//     for (var i = 1; i < gabe; i+=andy)
+//         r.push(fn(r[i - 1], this[i]));
+//     console.log(kate);
+var appendText =[]
+for (var i = 0; i < gabe; i+=andy) {
+while  > 0:
+    print n
+    n = n-1
+print 'Blastoff!'
+    // kate.push( "<br>"); //This adds each thing we want to append to the array in order.
+}
+
+//Out here we call the append once
+//Since we defined our variable as an array up there we join it here into a string
+appendText = appendText.join(" ");
+$("#sth").append(appendText);
+
+event.preventDefault();
+});
+});
