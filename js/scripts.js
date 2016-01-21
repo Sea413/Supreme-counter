55f5da7d61e2cb45ea9b66476ee09fa3e282fedc
Erik Tolentino and Sean Peerenboom <seanmp42@gmail.com>
2016-01-21 11:42:44 -0800

    Edit js

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,24 @@ $("form#question").submit(function(event) {
 
 var gabe = parseInt($("input#ct").val());
 var andy = parseInt($("input#cb").val());
-// var apple = []
-// for (var druid = 0; druid < gabe; druid+=andy) {
-//   apple.push("this is a test" + "<br>");
-//   // console.log("STEPPING");
-// }
-// apple = apple.join(" ");
-// $("#message").append(apple);
-// if (gabe === andy) {
-//   $("#message").show();
-// } else {
-//   $("#panic").show();
-// }
-// var kate = []
-// kate.prototype.accumulate =function(fn){
-//     var r = [this[0]];
-//     for (var i = 1; i < gabe; i+=andy)
-//         r.push(fn(r[i - 1], this[i]));
-//     console.log(kate);
-var appendText =[]
+var appendText = []; //We can define an array here if you need to play with the results in order or just use a string otherwise.
 for (var i = 0; i < gabe; i+=andy) {
-while  > 0:
-    print n
-    n = n-1
-print 'Blastoff!'
-    // kate.push( "<br>"); //This adds each thing we want to append to the array in order.
+    appendText.push(andy); //This adds each thing we want to append to the array in order.
 }
+console.log(appendText);
+
+var new_array = appendText.concat(); //Copy initial array
+
+for (var i = 1; i < appendText.length; i++) {
+  new_array[i] = new_array[i-1] + appendText[i];
+}
+
+console.log(new_array);
 
 //Out here we call the append once
 //Since we defined our variable as an array up there we join it here into a string
 appendText = appendText.join(" ");
-$("#sth").append(appendText);
+$("#sth").append(new_array + "<br>" );
 
 event.preventDefault();
 });
